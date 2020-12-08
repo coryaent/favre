d35dee2ccb7a6499f07e068b8a494780c50c4f7f
Steve Corya <steve@stevecorya.com>
2020-12-07 22:59:12 -0500

    listen on hostname

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,12 @@ USER_ID=${UID:-1000}
 GROUP_ID=${GID:-1000}
 
 # create new user and group with specified id's
-addgroup -g ${GROUP_ID} user
-adduser -u ${USER_ID} -D -H -G user user
+groupadd --gid ${GROUP_ID} user
+useradd --uid ${USER_ID} --gid ${GROUP_ID} --no-user-group user
 
 # change ownership of the mounted volume in the container
 chown -R user:user /sync
 
-exec /usr/sbin/csync2 -ii &
-exec "$@"
+printf "Starting Csync2 on %s...\n" $(hostname)
+gosu user:user /usr/sbin/csync2 -ii -N $(hostname) &
+gosu user:user "$@"
