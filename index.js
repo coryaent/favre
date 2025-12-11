104e35bf8d7badbceb87af0d251bc3b89356af1d
Stehpen Corya <stephen@corya.net>
2025-12-11 10:37:44 -0500

    add chokidar file watch and tidy up some syntax

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import { spawn, execFileSync } from 'node:child_process';
 import { writeFileSync } from 'node:fs';
+import { default as dns } from 'node:dns/promises';
 
 import { parse, escape, render } from 'mustache';
 import { globSync } from 'glob';
@@ -9,32 +10,33 @@ import chokidar from 'chokidar';
 
 // check for mandatory envorinmental variables
 if (!process.env.CSYNC2_PSK_FILE) {
-    console.error (new Date (), 'CSYNC2_PSK_FILE must be set');
-    process.exit (1);
+    console.error(new Date(), 'CSYNC2_PSK_FILE must be set');
+    process.exit(1);
 }
 
 // read multiple INCLUDE and EXCLUDE variables
 const includeGlobs = [];
 const excludes = [];
-for (let variable of Object.keys (process.env)) {
-    if (variable.startsWith ('CSYNC2_INCLUDE')) {
-        includeGlobs.push (process.env[variable]);
+for (let variable of Object.keys(process.env)) {
+    if (variable.startsWith('CSYNC2_INCLUDE')) {
+        includeGlobs.push(process.env[variable]);
     }
-    if (variable.startsWith ('CSYNC2_EXCLUDE')) {
-        excludes.push (process.env[variable]);
+    if (variable.startsWith('CSYNC2_EXCLUDE')) {
+        excludes.push(process.env[variable]);
     }
 }
 
 // parse globs from includes to pass to file watcher
-console.log (new Date (), 'Found', includeGlobs.length, 'globs to include');
+console.log (new Date(), 'Found', includeGlobs.length, 'globs to include');
 const includes = globSync(includeGlobs);
 console.log(new Date(), 'Found', includes.lengeth, 'paths/files to include');
 
-// no need to parse excludes, because these are passed to csync2
-console.log (new Date (), 'Found', excludes.length, 'patterns to exclude');
+// no need to parse excludes, because these are passed directly to csync2
+console.log(new Date(), 'Found', excludes.length, 'patterns to exclude');
 
 // start the csync2 daemon
-const csync2d = spawn ('csync2', ['-ii', process.env.CSYNC2_DAEMON_VERBOSITY, '-D', process.env.CSYNC2_DB_DIR]);
+let csync2d;
+csync2d = spawn ('csync2', ['-ii', process.env.CSYNC2_DAEMON_VERBOSITY, '-D', process.env.CSYNC2_DB_DIR]);
 // exit immediately if the daemon doesn't start successfully
 csync2d.on('error', (error) => {
     console.error(new Date(), error);
@@ -43,11 +45,13 @@ csync2d.on('error', (error) => {
 // sync once when the daemon successfully starts (the first thing it does is print to the console)
 csync2d.stdout.once('data', sync);
 // handle exit, stopping the client
-csync2d.on('exit', () => {
-    if (syncInterval) {
-        clearInterval(syncInterval);
-    }
-});
+csync2d.on('exit', exit);
+
+// create the file watcher with chokidar
+let watcher;
+watcher = chokidar.watch([includes]);
+// run sync on file changes
+watcher.on('all', sync);
 
 // mustache things
 const cfgTemplate = `
@@ -91,40 +95,60 @@ const cfg = {
 
 // main function
 async function sync () {
-    // fetch the list of nodes with all the gory details
-    let response = await fetch(`http://${process.env.FAVRE_DOCKER_HOST}/${process.env.FAVRE_DOCKER_API_VERSION}/nodes`);
-    // reduce to just hostnames
-    const nodes = (await response.json()).map(node => node.Description.Hostname);
+
+    // get peers by IP, hitting the docker dns endpoint
+    const taskLookups = [];
+    const aRecords = await dns.lookup(process.env.FAVRE_TASKS_ENDPOINT, { all: true });
+    for (let record of aRecords) {
+        taskLookups.push(dns.reverse(record.address));
+    }
+    // get resolvable task hosts
+    const endpoints = [];
+    const tasks = await Promise.all(taskLookups);
+    for (let task of tasks) {
+        // change reverse dns to match hostname
+        let remote = task[0].split('.').slice(0,3).toString().replaceAll(',','.');
+        console.debug(new Date(), 'Found remote', remote);
+        endpoints.push(remote);
+    }
+
     // cfg.hosts is from the last run (or empty if this is the first run)
     // if there is a host from the last run that is not in the new array, clean the database
     for (let host of cfg.hosts) {
-        if (!nodes.includes(host)) {
-            execFileSync ('csync2', ['-R', process.env.CSYNC2_CLIENT_VERBOSITY, '-D', process.env.CSYNC2_DB_DIR]);
+        if (!endpoints.includes(host)) {
+            execFileSync('csync2', ['-R', process.env.CSYNC2_CLIENT_VERBOSITY, '-D', process.env.CSYNC2_DB_DIR]);
             break;
         }
     }
-    // update config for template
-    cfg.hosts = nodes;
-    const configFile = render(cfgTemplate, cfg);
-    writeFileSync(`${process.env.CSYNC2_SYSTEM_DIR}/csync2.cfg`, configFile);
-    // run the synchronization operation
-    execFileSync ('csync2', ['-x', '-r', process.env.CSYNC2_CLIENT_VERBOSITY, '-D', process.env.CSYNC2_DB_DIR]);
+    
+    // run this immediately so that the event loop doesn't interfere
+    // i.e. run this synchronously before it can be called again
+    process.nextTick(() => {
+        // update config for template
+        cfg.hosts = endpoints;
+        const configFile = render(cfgTemplate, cfg);
+        writeFileSync(`${process.env.CSYNC2_SYSTEM_DIR}/csync2.cfg`, configFile);
+        // run the synchronization operation
+        execFileSync('csync2', ['-x', '-r', process.env.CSYNC2_CLIENT_VERBOSITY, '-D', process.env.CSYNC2_DB_DIR]);
+    });
 }
-
-// run sync function periodically (won't go faster than every second)
-syncInterval = setInterval (sync, Number.parseInt(process.env.CSYNC2_SYNC_INTERVAL) * 1000 || 1000);
 
 // clean exit, stopping both the client and the server
 function exit(signal) {
     // acknowledge receipt
     console.log(new Date(), signal, 'received');
     console.log(new Date(), 'Exiting...');
-    // stop the client running at an interval
-    if (syncInterval) {
-        clearInterval(syncInterval);
+
+    // remove the watcher
+    if (watcher) {
+        // this method is async, but there is no need to await it
+        watcher.close();
     }
+
     // stop the daemon
-    csync2d.kill();
+    if (csync2d) {
+        csync2d.kill();
+    }
 }
 
 process.on('SIGINT', exit);
