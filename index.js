de6b65b343ac1ff4696f1cfddcf254693d1f0213
Stehpen Corya <stephen@corya.net>
2025-12-30 08:53:09 -0500

    make sync retriable instead of pRetry

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { default as dns } from 'node:dns/promises';
 import Mustache from 'mustache';
 import { globSync } from 'glob';
 import chokidar from 'chokidar';
-import pRetry from 'p-retry';
+import { makeRetriable } from 'p-retry';
 
 // check for mandatory envorinmental variables
 if (!process.env.CSYNC2_PSK_FILE) {
@@ -55,7 +55,7 @@ csync2d.once('spawn', () => {
         // create the file watcher with chokidar
         watcher = chokidar.watch(includes);
         // run sync on file changes
-        watcher.on('all', pRetry(sync));
+        watcher.on('all', makeRetriable(sync));
     }, 5000);
 });
 // handle exit, stopping the client
